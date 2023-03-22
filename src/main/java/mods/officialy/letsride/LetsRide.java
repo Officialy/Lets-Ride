af2ad91ce849e545afdf6e8024d63c22f484cae7
Max <mcmad.mm@gmail.com>
2023-03-22 03:04:45 +0000

    Start of the project push - Ohboy

---
+++ src/main/java/mods/officialy/letsride/LetsRide.java
@@ -0,0 +1,71 @@
+package mods.officialy.letsride;
+
+import com.mojang.logging.LogUtils;
+import mods.officialy.letsride.common.block.LRBlocks;
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.CreativeModeTabEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(LetsRide.MODID)
+public class LetsRide {
+
+    public static final String MODID = "letsride";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public LetsRide() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        MinecraftForge.EVENT_BUS.register(this);
+        modEventBus.addListener(this::commonSetup);
+
+        LRBlocks.BLOCKS.register(modEventBus);
+        LRBlocks.ITEMS.register(modEventBus);
+        // Register the items to a creative tab
+        modEventBus.addListener(this::addCreative);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+
+    }
+
+    private void addCreative(CreativeModeTabEvent.BuildContents event)
+    {
+        if (event.getTab() == CreativeModeTabs.BUILDING_BLOCKS){
+           /* for (var block : LRBlocks.BLOCKS.getEntries())
+            {
+                event.accept(block::get);
+            }*/
+        }
+
+    }
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
+
+        }
+    }
+}
---
+++ src/main/java/mods/officialy/letsride/common/block/LRBlocks.java
@@ -0,0 +1,27 @@
+package mods.officialy.letsride.common.block;
+
+import mods.officialy.letsride.LetsRide;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.function.Supplier;
+
+public class LRBlocks {
+
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, LetsRide.MODID);
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, LetsRide.MODID);
+
+    public static final RegistryObject<Block> EXAMPLE_BLOCK = register("example_block", () -> new Block(BlockBehaviour.Properties.of(Material.STONE)));
+
+    public static <T extends Block> RegistryObject<T> register(String name, Supplier<T> block) {
+        RegistryObject<T> object = BLOCKS.register(name, block);
+        ITEMS.register(name, () -> new BlockItem(object.get(), new Item.Properties()));
+        return object;
+    }
+}
